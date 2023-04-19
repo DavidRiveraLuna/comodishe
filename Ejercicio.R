0538fa3fd783a0fa7df3f214a8c897db766a9c0a
David <david..rivera@iest.edu.mx>
2023-04-19 16:48:32 -0500

    Modificacion codigo R

--- Ejercicio.R
+++ Ejercicio.R
@@ -2,6 +2,6 @@ Data_Frame <- data.frame (
   Años = c(10,20,21),
   Mayor_de_edad = c(FALSE, TRUE, TRUE),
   Fecha = as.Date(c("2013-01-01", "2003-01-01", "2002-01-01")),
-  name = c("Jorgito", "Josue", "Sebastian")
+  name = c("Gerardo", "Ray", "Sebastian")
 )
 summary(Data_Frame)
